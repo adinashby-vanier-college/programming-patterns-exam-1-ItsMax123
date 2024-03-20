efac8498ae4df51a1cfb386773e6fbe70c6d2716
Maxime Mir-Orefice <2319522@edu.vaniercollege.qc.ca>
2024-03-20 14:30:49 -0400

    first

--- src/main/java/com/prog2/labs/ExamOneSec1And2.java
+++ src/main/java/com/prog2/labs/ExamOneSec1And2.java
@@ -24,11 +24,54 @@ public class ExamOneSec1And2 {
     /**
      * Please refer to the README file for question(s) description
      */
-    public static List<Integer> partitionLabels(String s) {
-        return null;
+        public static List<Integer> partitionLabels(String s) {
+        HashMap<Character, Integer> last = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            last.put(s.charAt(i), i);
+        }
+        List result = new ArrayList<Integer>();
+        int size = 1;
+        int endPoint = 0;
+        int tempPoint;
+        for (int i = 0; i < s.length(); i++) {
+            tempPoint = last.get(s.charAt(i));
+            if (tempPoint > endPoint) {
+                endPoint = tempPoint;
+            }
+            if (i == endPoint) {
+                result.add(size);
+                size = 1;
+            } else {
+                size++;
+            }
+        }
+        return result;
     }
 
+    public static void recursiveTraverse(TreeNode node, int layer, ArrayList<ArrayList<Integer>> layers) {
+        System.out.println(layers.get(layer));
+        if (layers.get(layer) == null) {
+            layers.add(new ArrayList<>());
+        }
+        layers.get(layer).add(node.val);
+        if (node.left != null) {
+            recursiveTraverse(node.left, layer + 1, layers);
+        }
+        if (node.right != null) {
+            recursiveTraverse(node.right, layer + 1, layers);
+        }
+    }
+        
     public static int[] traverseTree(TreeNode root) {
+        TreeNode node = root;
+        ArrayList<ArrayList<Integer>> layers = new ArrayList<>();
+        int layer = 0;
+        recursiveTraverse(node, 0, layers);
+        //while (node.left == null && node.right == null) {
+            
+        //}
+        System.out.println("test");
+        
         return null;
     }
 
